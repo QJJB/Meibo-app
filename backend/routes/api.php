cdec7a2886a024fdaf2435217a07edcbb1527636
Bastien Morrier <bastien.morrier@gmail.com>
2025-04-01 12:15:53 +0200

    test branch

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,3 +13,6 @@ Route::middleware(['auth:sanctum'])->get('/projects/{id}', [ProjectController::c
 Route::middleware(['auth:sanctum'])->post('/projects', [ProjectController::class, 'create']);
 Route::middleware(['auth:sanctum'])->put('/projects/{id}', [ProjectController::class, 'update']);
 Route::middleware(['auth:sanctum'])->delete('/projects/{id}', [ProjectController::class, 'delete']);
+// coucou
+
+Route::middleware(['auth:sanctum'])->delete('/projects/{id}', [ProjectController::class, 'delete']);
